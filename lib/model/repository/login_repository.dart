fa253e0b047ce23e8a0c11926a16c95263ab9a57
Lucas Tonussi <lucastonussi@gmail.com>
2020-06-04 18:40:44 -0300

    a

--- lib/model/repository/login_repository.dart
+++ lib/model/repository/login_repository.dart
@@ -1,14 +1,12 @@
 import 'package:salesforce/model/user_model.dart';
-import 'package:salesforce/providers/db_provider.dart';
 import 'package:salesforce/providers/user_provider.dart';
 
 //Retira a responsabilidade da model, ou seja, a classe deve se ter apenas uma responsabilidade
 class LoginRepository {
   Future<bool> doLogin(Usuario model) async {
     var apiUsuario = UsuarioApiProvider();
-    Future<List<Usuario>> list = DBProvider.db.getAllUsuario();
-    //List<Usuario> list = await apiUsuario.getAllUsuarios();
-
+    List<Usuario> list = await apiUsuario.getAllUsuarios();
+    list.map((user) => print(list[0].nome)).toList();
     return model.nome == 'a' && model.senha == 'a';
   }
 }
--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -100,7 +100,7 @@ class DBProvider {
 
   // Insert usuario on database
   createUsuario(Usuario newUsuario) async {
-    await deleteAllUsuario();
+    //await deleteAllUsuario();
     final db = await database;
     final res = await db.insert('usuario', newUsuario.toJson());
     return res;
